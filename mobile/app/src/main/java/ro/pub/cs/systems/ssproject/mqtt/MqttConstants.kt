5524f189e377a114a91fe903c960c49cf01f703d
Airinei Andrei <acairinei17@gmail.com>
2026-05-04 20:09:59 +0300

    Add constants

--- mobile/app/src/main/java/ro/pub/cs/systems/ssproject/mqtt/MqttConstants.kt
+++ mobile/app/src/main/java/ro/pub/cs/systems/ssproject/mqtt/MqttConstants.kt
@@ -1,11 +1,18 @@
 package ro.pub.cs.systems.ssproject.mqtt
-
+ 
 object MqttConstants {
     const val TAG = "MqttHandler"
     const val TOPIC_IMAGES = "ssproject/images"
     const val TOPIC_COMMANDS = "ssproject/commands"
-
+ 
     const val CMD_CAPTURE = "CAPTURE"
     const val CMD_START_LIVE = "START-LIVE"
     const val CMD_STOP_LIVE = "STOP-LIVE"
+ 
+    // TLS
+    const val BKS_STORE_TYPE = "BKS"
+    const val TLS_PROTOCOL = "TLSv1.2"
+    const val KEYSTORE_PASSWORD = "changeit"
+    const val TRUSTSTORE_PASSWORD = "changeit"
+    const val DEFAULT_TLS_PORT = "8883"
 }
